b3f02633ca42bbf9893e8f50d6444aac59c0cfcc
Cedric UwewU <70793428+cedricmatthewverdida@users.noreply.github.com>
2022-04-15 14:06:08 +0700

    Update index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,1 +1,25 @@
 
+
+
+import { GetterTree, ActionTree, MutationTree } from 'vuex'
+
+export const state = () => ({
+  user: [] as Object[]
+})
+
+export type RootState = ReturnType<typeof state>
+
+export const getters: GetterTree<RootState, RootState> = {
+    user: state => state.user,
+}  
+
+export const mutations: MutationTree<RootState> = {
+    SET_USER: (state, user: Object[]) => (state.user = user),
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+    async nuxtServerInit  ({ commit }, { req }) {
+        console.log("Hello")
+    }
+}
+
