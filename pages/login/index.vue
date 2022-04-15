7d31c2023a712e020584044a6c068df97d743acc
Cedric UwewU <70793428+cedricmatthewverdida@users.noreply.github.com>
2022-04-15 15:47:10 +0700

    Update index.vue

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hero min-h-screen bg-base-200">
+    <!-- <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col lg:flex-row-reverse">
         <div class="text-center lg:text-left">
         <h1 class="text-5xl font-bold">Login now!</h1>
@@ -7,7 +7,12 @@
         </div>
         <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div class="card-body">
-            
+            <div class="card-title">
+                <h1>
+                    Login
+                </h1>
+            </div>
+
             <Input
                 :option="{
                     label: 'Email / Username',
@@ -32,6 +37,35 @@
         </div>
         </div>
     </div>
+    </div> -->
+    <div class="hero min-h-screen bg-base-200">
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure><img src="https://api.lorem.space/image/album?w=400&h=400" alt="Album"></figure>
+            <div class="card-body">
+                <h2 class="card-title">Connect with Sam's Store</h2>
+                <Input
+                    :option="{
+                        label: 'Email / Username',
+                        placeholder: 'Enter your email / username',
+                        type: 'email',
+                    }"
+                    v-model="credential.email"
+                />
+
+                <Input
+                    :option="{
+                        label: 'Password',
+                        placeholder: 'Enter your password',
+                        type: 'password',
+                    }"
+                    v-model="credential.password"
+                />
+
+                <div class="form-control mt-6">
+                    <button class="btn btn-primary" @click="login">Login</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
